9157128e436cec92da5451334d4b340b4e0a58a8
mohaned-omer <mohanedomerhamed001234@gmail.com>
2026-05-22 16:46:42 +0200

    making the comment 'hidden' by defualt

--- lua/custom/colors/gruvbox.lua
+++ lua/custom/colors/gruvbox.lua
@@ -7,6 +7,7 @@ return {
       -- Set options BEFORE the colorscheme command
       vim.g.gruvbox_material_background = 'Warm'
       vim.cmd.colorscheme 'gruvbox-material'
+      vim.api.nvim_set_hl(0, 'Comment', { fg = '#707074', ctermfg = 245, bold = false, italic = false })
     end,
   },
 }
